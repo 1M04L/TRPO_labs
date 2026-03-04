f0fbe2d180761e52bc9716edcb1a2af5f2227abb
1M04L <mikhailulianenko@gmail.com>
2026-03-04 11:51:09 +0700

    создается filemanager .написаны функции , запускающие мониторинг

--- main.cpp
+++ main.cpp
@@ -1,25 +1,54 @@
 #include <QCoreApplication>
-#include <QLocale>
-#include <QTranslator>
 #include <QStringList>
 #include <QTimer>
 #include <iostream>
 #include "filemanager.h"
 #include "logger.h"
 
+void printUsage()
+{
+    Logger::instance().info("Использование: FileMonitor <путь_к_файлу1> [путь_к_файлу2] ...");
+    Logger::instance().info("Пример: FileMonitor C:\\test.txt D:\\document.txt");
+    Logger::instance().info("");
+    Logger::instance().info("Программа будет следить за существованием и размером указанных файлов.");
+    Logger::instance().info("Для выхода нажмите Ctrl+C");
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "trpo1_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
+    // экземпляр логгера
+    Logger& logger = Logger::instance();
+
+    logger.info("=== ФАЙЛОВЫЙ МОНИТОР ===");
+
+    // Проверка аргументов командной строки
+    QStringList args = a.arguments();
+    if (args.size() < 2) {
+        logger.error("Ошибка: не указаны файлы для наблюдения!");
+        printUsage();
+        return 1;
     }
 
+    // Удаляем первый аргумент (имя программы)
+    args.removeFirst();
+
+    // Создаем менеджер файлов
+    FileManager manager;
+
+    for (const QString& filePath : args) {
+        manager.addFile(filePath);
+    }
+
+    manager.printAllFilesInfo();
+
+
+    manager.startMonitoring(100);
+
+    logger.info("Мониторинг запущен. Для выхода нажмите Ctrl+C");
+
+    //  цикл обработки событий
     return a.exec();
 }
+
