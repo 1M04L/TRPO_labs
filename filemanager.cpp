dade7c543f2c2f0f99d2f4cb76de20f18e0bf4da
1M04L <mikhailulianenko@gmail.com>
2026-03-04 11:53:24 +0700

    созданы конструктор и деструктор для файлогового менеджера

--- filemanager.cpp
+++ filemanager.cpp
@@ -1,6 +1,24 @@
 #include "filemanager.h"
+#include "logger.h"
+#include <QCoreApplication>
+#include <algorithm>
 
-filemanager::filemanager()
+FileManager::FileManager(QObject* parent)
+    : QObject(parent)
 {
+    m_timer.setInterval(100);
+    connect(&m_timer, &QTimer::timeout, this, &FileManager::checkAllFiles);
 
+    Logger::instance().info("Менеджер файлов инициализирован");
+}
+
+FileManager::~FileManager()
+{
+    stopMonitoring();
+
+    // Очищ вектор с наблюдателями
+    qDeleteAll(m_watchers);
+    m_watchers.clear();
+
+    Logger::instance().info("Менеджер файлов завершил работу");
 }
