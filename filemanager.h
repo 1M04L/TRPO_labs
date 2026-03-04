992532c50dd80b6a14e75964012943a229b89532
1M04L <mikhailulianenko@gmail.com>
2026-03-04 11:43:46 +0700

    обьявлены методы класса FileManager

--- filemanager.h
+++ filemanager.h
@@ -1,11 +1,32 @@
 #ifndef FILEMANAGER_H
 #define FILEMANAGER_H
 
+#include <QObject>
+#include <QVector>
+#include <QTimer>
+#include "filewatcher.h"
 
-class filemanager
+class FileManager : public QObject
 {
+    Q_OBJECT
+
 public:
-    filemanager();
+    explicit FileManager(QObject* parent = nullptr);
+    ~FileManager();
+
+    void addFile(const QString& filePath);
+    void removeFile(const QString& filePath);
+    void printAllFilesInfo() const;
+    void startMonitoring(int intervalMs = 100);
+    void stopMonitoring();
+
+private slots:
+    void checkAllFiles();
+
+private:
+    QVector<FileWatcher*> m_watchers;
+    QTimer m_timer;
 };
 
 #endif // FILEMANAGER_H
+
