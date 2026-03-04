0b13a182cc1b51ae8457d8c02662e53588b42377
1M04L <mikhailulianenko@gmail.com>
2026-03-04 11:44:46 +0700

    обьявлены методы класса filewatcher

--- filewatcher.h
+++ filewatcher.h
@@ -1,11 +1,40 @@
 #ifndef FILEWATCHER_H
 #define FILEWATCHER_H
 
+#include <QObject>
+#include <QFileInfo>
+#include <QTimer>
+#include <QString>
 
-class filewatcher
+class FileWatcher : public QObject
 {
+    Q_OBJECT
+
 public:
-    filewatcher();
+    explicit FileWatcher(const QString& filePath, QObject* parent = nullptr);
+    ~FileWatcher();
+
+    QString getFilePath() const;
+    bool isFileExists() const;
+    qint64 getFileSize() const;
+
+public slots:
+    void checkFile();
+
+signals:
+    void fileCreated(const QString& path, qint64 size);
+    void fileDeleted(const QString& path);
+    void fileChanged(const QString& path, qint64 newSize);
+    void fileRestored(const QString& path, qint64 size);
+
+private:
+    QString m_filePath;
+    QFileInfo m_fileInfo;
+    bool m_lastExists;
+    qint64 m_lastSize;
+
+    bool updateFileInfo();
+    void processChanges(bool exists, qint64 size);
 };
 
-#endif // FILEWATCHER_H
+#endif
